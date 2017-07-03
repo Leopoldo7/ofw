29dd0c2e7e66303092aa5cc508910148dc04d2d8
Antonio90 <riccardiantonio90@libero.it>
2017-07-03 16:57:08 +0200

    Vedi come si recuperano i dati

--- src/pages/panel/panel.ts
+++ src/pages/panel/panel.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Events } from 'ionic-angular';
 import {SignalK} from '../signalk/signalk';
 import {ServerFormPage} from '../serverform/serverform';
 
@@ -11,12 +11,24 @@ export class PanelPage {
 
   constructor(
     public navCtrl: NavController,
-    public signalK: SignalK
-  ){}
+    public signalK: SignalK,
+    private events: Events
+  ){
+    this.events.subscribe('ws:message', (jsonData) => {
+      this.handleData(jsonData);
+    });
+
+  }
 
 
   obtainServerAddrManually() {
     this.navCtrl.push(ServerFormPage);
+  }
+
+
+  handleData(jsonData){
+      console.info("Arrivato un dato")
+      console.info(jsonData)
   }
 
 }

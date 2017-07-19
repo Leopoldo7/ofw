cf1628d69b202df59d3984a78ff629b50c8bb77b
Antonio90 <riccardiantonio90@libero.it>
2017-07-19 11:19:07 +0200

    Aggiunta libreria steelseries.js Usati i nuovi gadget

--- src/pages/panel/panel.ts
+++ src/pages/panel/panel.ts
@@ -1,19 +1,25 @@
 import { Component, Input} from '@angular/core';
+import { OnInit } from '@angular/core';
 import { NavController, Events } from 'ionic-angular';
 import {SignalK} from '../signalk/signalk';
 import {ServerFormPage} from '../serverform/serverform';
+
+declare var steelseries:any;
 
 @Component({
   selector: 'page-panel',
   templateUrl: 'panel.html'
 })
-export class PanelPage {
+export class PanelPage implements OnInit {
 
   @Input() courseover = 0;
   @Input() speedover = 0;
   @Input() depthbelowTransducer = 0;
   @Input() magneticVariation = 0;
   @Input() windSpeed = 0;
+
+  public canvasSOG:any;
+  public multiPosition:any;
 
   @Input() valoreInput: string = "10.00";
            
@@ -27,6 +33,10 @@ export class PanelPage {
     this.events.subscribe('ws:message', (jsonData) => {
       this.handleData(jsonData);
     });
+  }
+
+  ngOnInit(){
+    this.initGadget();
   }
 
   obtainServerAddrManually() {
@@ -52,8 +62,11 @@ export class PanelPage {
             if(json.updates[i].values){
               for(var k in json.updates[i].values){
                 var curr = json.updates[i].values[k];
-                if(curr.path === "navigation.courseOverGroundTrue"){ 
-                    this.courseover = curr.value;
+                if(curr.path === "navigation.courseOverGroundTrue"){ //environment.outside.temperature --> navigation.courseOverGroundTrue
+                    //this.courseover = curr.value;
+                    this.canvasSOG.setValue(curr.value); //Setta il valore di  SOG
+                    this.multiPosition.setValue(curr.value); // Setta il primo valore del Display con due parametri
+                    this.multiPosition.setAltValue(curr.value + 10)  // Setta il secondo valore del Display con due parametri
                 } else if(curr.path === "navigation.speedOverGround"){
                     this.speedover = curr.value;
                 } else if(curr.path === "depth.belowTransducer"){
@@ -69,6 +82,32 @@ export class PanelPage {
        }
     }
   }
+
+
+    initGadget(){
+
+      this.canvasSOG = new steelseries.DisplaySingle('canvasSOG', {
+               width: 150,
+               height: 50,
+               unitString: "m/s",
+               unitStringVisible: true,
+               headerString: "SOG",
+               lcdDecimals: 1,
+               headerStringVisible: true,
+               lcdColor: steelseries.LcdColor.BEIGE
+            });
+
+      this.multiPosition = new steelseries.DisplayMulti('canvasPosition', {
+          width: 150,
+          height: 50,
+          headerString: "POSITION",
+          headerStringVisible: true,
+          unitString: "Lat/Lon",
+          unitStringVisible: true,
+          lcdColor: steelseries.LcdColor.BEIGE
+      })
+
+    }
 
 }
 

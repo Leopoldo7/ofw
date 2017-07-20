cab2375f6bb139235cfeb8499101205a506ec517
Antonio90 <riccardiantonio90@libero.it>
2017-07-20 17:21:37 +0200

    Ora la nave compare

--- src/pages/maps/maps.ts
+++ src/pages/maps/maps.ts
@@ -15,27 +15,32 @@ declare var ol:any;
 export class MapsPage implements OnInit {
 
    public map:any;
+   public zoom:number = 15;
+   public imageMarker:string = 'assets/img/maps/ship.png';
+   public fromProjection:string = "EPSG:4326";
+   public toProjection:string = "EPSG:3857";
+
+   public features = new ol.Collection();
+
+
    public view:any = new ol.View({
-                          center: ol.proj.transform([14.26824, 40.83033], 'EPSG:4326', 'EPSG:3857'),
-                          zoom: 13
+                          center: ol.proj.transform([14.26824, 40.83033], this.fromProjection, this.toProjection),
+                          zoom: this.zoom
                         });
 
-  public geolocation:any = new ol.Geolocation({
-                                projection: this.view.getProjection()
-                              }).on("change",function(){
-                                
-                              });
+  public geolocation = new ol.Geolocation({
+        tracking: true
+      });
 
-  public fill = new ol.style.Fill();
+  
   public style = new ol.style.Style({
-            fill: this.fill,
-            stroke: new ol.style.Stroke({
-              color: '#333',
-              width: 2
+            image: new ol.style.Icon({
+              src: this.imageMarker,
+              anchor: [0.5, 0.5],
+              scale: 0.30
             })
       });
     
-  public features = new ol.Collection();
 
   public vectorSource = new ol.source.Vector({
     features: this.features
@@ -82,16 +87,66 @@ export class MapsPage implements OnInit {
                 ]),
     });
 
-    //this.vectorSource.
+    //Prende la posizione attuale
+    if(this.geolocation && this.geolocation.getTracking()){
+     
+      /*var positionActual = this.geolocation.getPosition();
+      
+      if(positionActual){
+        this.view.setCenter([parseFloat(positionActual[0]), parseFloat(positionActual[1])]);
+        this.view.setZoom(this.zoom);
+      }*/
+      var viewer = this.view;
+      var zoommer = this.zoom;
+      var fmProj = this.fromProjection;
+      var toProj = this.toProjection;
+      /* NON so se va bene per una nave che va sempre in movimento */
+      this.geolocation.on('change', function() {
+        
+          var p = this.getPosition();
+          viewer.setCenter(ol.proj.transform([p[0],p[1]], fmProj, toProj));
+          viewer.setZoom(zoommer);
+        
+      });
+
+    }
+
 
   }
 
 
+  handleData(json){
+      if(json){
+      json = JSON.parse(json);
+       if(json.updates){
+          for(var i in json.updates){
+            if(json.updates[i].values){
+              for(var k in json.updates[i].values){
+                var curr = json.updates[i].values[k];
 
+                if(curr.path === "navigation.position"){
 
+                    var pos = curr.value;
+                    if(pos.longitude && pos.latitude){
 
-  handleData(data){
-
+                      var feature = new ol.Feature({
+                          geometry: new ol.geom.Point(ol.proj.transform([pos.longitude, pos.latitude], this.fromProjection, this.toProjection)),
+                          name: 'La mia nave'
+                        })
+                        //{"longitude":-122.833183,"latitude":48.234501}
+                        feature.setId("mia_nave");
+                        if(this.vectorSource.getFeatureById("mia_nave"))
+                          this.vectorSource.removeFeature(this.vectorSource.getFeatureById("mia_nave"));
+                        this.vectorSource.addFeature(feature);
+                        this.view.setCenter(ol.proj.transform([pos.longitude,pos.latitude], this.fromProjection, this.toProjection));
+                        this.view.setZoom(this.zoom);
+                    }
+                }
+              }
+            }
+          }
+        }
+      }
   }
 
 
